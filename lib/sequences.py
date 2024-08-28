5e052cc887e7f0e56c5dd6d18d4b96102f4e8d3b
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-28 12:56:58 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    seq = []
+    if length == 0:
+        print(seq)
+        return
+
+    if length >= 1:
+        seq.append(0)
+
+    if length >= 2:
+        seq.append(1)
+
+    for i in range(2, length):
+        next_num = seq[-1] + seq[-2]
+        seq.append(next_num)
+        
+    print(seq)
+
